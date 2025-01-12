8bb94675149eaf165e2f8c6b007fd5cce99cec4e
Anggiat Mora Simamora <anggiat17@gmail.com>
2025-01-12 16:06:53 +0700

    add pio

---
+++ alat/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/anggi/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/anggi/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/anggi/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
